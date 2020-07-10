4be27cbe9325782734747f3b559f58a922295054
Miracle <122094709@qq.com>
2020-07-10 15:53:59 +0800

    wav文件的播放

--- app/src/main/java/com/example/videodemo/AudioRecordSample/AudioRecordActivity.java
+++ app/src/main/java/com/example/videodemo/AudioRecordSample/AudioRecordActivity.java
@@ -2,9 +2,13 @@ package com.example.videodemo.AudioRecordSample;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.media.AudioAttributes;
 import android.media.AudioFormat;
+import android.media.AudioManager;
 import android.media.AudioRecord;
+import android.media.AudioTrack;
 import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -20,7 +24,9 @@ import androidx.core.content.ContextCompat;
 
 import com.example.videodemo.R;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -64,6 +70,8 @@ public class AudioRecordActivity extends AppCompatActivity implements View.OnCli
     private int recordBufSize = 0; //recordBuffer的大小
     private boolean isRecording;
     private Button btnConvert;//音频格式转换
+    private Button btnPlay, btnStop; //播放 停止音频
+    private AudioTrack audioTrack;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -75,6 +83,10 @@ public class AudioRecordActivity extends AppCompatActivity implements View.OnCli
         btnStopRecord.setOnClickListener(this);
         btnConvert = findViewById(R.id.btn_convert);
         btnConvert.setOnClickListener(this);
+        btnPlay = findViewById(R.id.btn_start_play);
+        btnPlay.setOnClickListener(this);
+        btnStop = findViewById(R.id.btn_stop_play);
+        btnStop.setOnClickListener(this);
         checkPermissmions();
     }
 
@@ -125,9 +137,20 @@ public class AudioRecordActivity extends AppCompatActivity implements View.OnCli
                 }
                 pcmToWaUtils.pcmToWav(pcmFile.getAbsolutePath(), wavFile.getAbsolutePath());
                 break;
+            case R.id.btn_start_play:
+                playInModeStream();
+//                playInModeStatic();
+                break;
+            case R.id.btn_stop_play:
+                stopPlay();
+                break;
         }
     }
 
+
+    /**
+     * 开始录音
+     */
     private void startRecord() {
         final int minBufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE_INHZ, CHANNEL_CONFIG, AUDIO_FORMAT);
         audioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_RATE_INHZ, CHANNEL_CONFIG, AUDIO_FORMAT, minBufferSize);
@@ -171,6 +194,9 @@ public class AudioRecordActivity extends AppCompatActivity implements View.OnCli
         }).start();
     }
 
+    /**
+     * 停止录音
+     */
     private void stopRecord() {
         isRecording = false;
         // 释放资源
@@ -178,6 +204,118 @@ public class AudioRecordActivity extends AppCompatActivity implements View.OnCli
             audioRecord.stop();
             audioRecord.release();
             audioRecord = null;
+        }
+    }
+
+    /**
+     * 播放，使用stream模式
+     */
+    private void playInModeStream() {
+        /*
+         * SAMPLE_RATE_INHZ 对应pcm音频的采样率
+         * channelConfig 对应pcm音频的声道
+         * AUDIO_FORMAT 对应pcm音频的格式
+         * */
+        int channelConfig = AudioFormat.CHANNEL_OUT_MONO;
+        final int minBufferSize = AudioTrack.getMinBufferSize(SAMPLE_RATE_INHZ, channelConfig, AUDIO_FORMAT);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            audioTrack = new AudioTrack(
+                    new AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_MEDIA)
+                            .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                            .build(),
+                    new AudioFormat.Builder()
+                            .setEncoding(AUDIO_FORMAT)
+                            .setChannelMask(channelConfig)
+                            .build(),
+                    minBufferSize, AudioTrack.MODE_STREAM, AudioManager.AUDIO_SESSION_ID_GENERATE);
+        }
+        audioTrack.play();
+
+        File file = new File(Environment.getExternalStorageDirectory() + "/test.pcm");
+        try {
+            final FileInputStream fileInputStream = new FileInputStream(file);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        byte[] tempBuffer = new byte[minBufferSize];
+                        while (fileInputStream.available() > 0) {
+                            int readCount = fileInputStream.read(tempBuffer);
+                            if (readCount == AudioTrack.ERROR_INVALID_OPERATION || readCount == AudioTrack.ERROR_BAD_VALUE) {
+                                continue;
+                            }
+                            if (readCount != 0 && readCount != -1) {
+                                audioTrack.write(tempBuffer, 0, readCount);
+                            }
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }).start();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void playInModeStatic() {
+        // static模式，需要将音频数据一次性write到AudioTrack的内部缓冲区
+        new AsyncTask<Void, Void, byte[]>() {
+
+            @Override
+            protected byte[] doInBackground(Void... voids) {
+                try {
+                    File file = new File(Environment.getExternalStorageDirectory() + "/test.wav");
+                    FileInputStream fileInputStream = new FileInputStream(file);
+                    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                    byte[] temp = new byte[1024];
+                    int count;
+                    while ((count = fileInputStream.read(temp)) != -1) {
+                        outputStream.write(temp, 0, count);
+                    }
+                    return outputStream.toByteArray();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(byte[] audioData) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    audioTrack = new AudioTrack(
+                            new AudioAttributes.Builder()
+                                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                                    .build(),
+                            new AudioFormat.Builder().setSampleRate(22050)
+                                    .setEncoding(AudioFormat.ENCODING_PCM_8BIT)
+                                    .setChannelMask(AudioFormat.CHANNEL_OUT_MONO)
+                                    .build(),
+                            audioData.length,
+                            AudioTrack.MODE_STATIC,
+                            AudioManager.AUDIO_SESSION_ID_GENERATE);
+                }
+                Log.d(TAG, "Writing audio data...");
+                audioTrack.write(audioData, 0, audioData.length);
+                Log.d(TAG, "Starting playback");
+                audioTrack.play();
+                Log.d(TAG, "Playing");
+            }
+        }.execute();
+    }
+
+    /**
+     * 停止播放
+     */
+    private void stopPlay() {
+        if (audioTrack != null) {
+            Log.e(TAG, "Stop");
+            audioTrack.stop();
+            audioTrack.release();
         }
     }
 }
