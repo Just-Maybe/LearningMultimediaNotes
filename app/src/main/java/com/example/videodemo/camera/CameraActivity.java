a20ab2c691430dcbc41e64cfede8686269734342
Miracle <122094709@qq.com>
2020-07-10 18:45:09 +0800

    Camera 闪退问题

--- app/src/main/java/com/example/videodemo/camera/CameraActivity.java
+++ app/src/main/java/com/example/videodemo/camera/CameraActivity.java
@@ -3,17 +3,17 @@ package com.example.videodemo.camera;
 import android.graphics.ImageFormat;
 import android.hardware.Camera;
 import android.os.Bundle;
-import android.view.Surface;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.videodemo.BasePermissionsActivity;
 import com.example.videodemo.R;
 
 public class CameraActivity extends BasePermissionsActivity implements SurfaceHolder.Callback {
+    private static final String TAG = CameraActivity.class.getSimpleName();
     private SurfaceView surfaceView;
     Camera camera;
 
@@ -27,29 +27,23 @@ public class CameraActivity extends BasePermissionsActivity implements SurfaceHo
     private void initView() {
         surfaceView = findViewById(R.id.surface);
         surfaceView.getHolder().addCallback(this);
-        camera = Camera.open();
-        camera.setPreviewCallback(new Camera.PreviewCallback() {
-            @Override
-            public void onPreviewFrame(byte[] data, Camera camera) {
-
-            }
-        });
     }
 
     // surface 预览
     @Override
     public void surfaceCreated(SurfaceHolder holder) {
         try {
+            camera = Camera.open();
             Camera.Parameters parameters = camera.getParameters();
-            parameters.set("orientation", "portrait");  //竖屏预览
             //Android 中Google支持的 Camera Preview Callback的YUV常用格式有两种：
             // 一个是NV21，一个是YV12。Android一般默认使用YCbCr_420_SP的格式（NV21）。
             parameters.setPreviewFormat(ImageFormat.NV21);
-            camera.setDisplayOrientation(90);
+            parameters.set("orientation", "portrait");  //竖屏预览
+            camera.setDisplayOrientation(90);   // 旋转90度
             camera.setParameters(parameters);
-
             camera.setPreviewDisplay(holder);
             camera.startPreview();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -62,6 +56,8 @@ public class CameraActivity extends BasePermissionsActivity implements SurfaceHo
 
     @Override
     public void surfaceDestroyed(SurfaceHolder holder) {
+        camera.stopPreview();
         camera.release();
     }
+
 }
